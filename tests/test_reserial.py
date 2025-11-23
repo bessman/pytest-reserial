1e2e211eeec9a90466dc476a0daff132e58dd9b5
Eli Fine <eli88fine@gmail.com>
2025-11-23 08:39:59 -0500

    Feat: support `serial_for_url` for RFC2217 servers (#31)

--- tests/test_reserial.py
+++ tests/test_reserial.py
@@ -1,28 +1,40 @@
 import json
 
+import pytest
 from serial import Serial
+from serial.rfc2217 import Serial as RFC2217Serial
 
 TEST_RX = b"\x01"
 TEST_RX_ENC = "AQ=="
 TEST_TX = b"\x02"
 TEST_TX_ENC = "Ag=="
-TEST_FILE = f"""
+STANDARD_SERIAL_CONNECTION_INIT = 'serial.Serial(port="/dev/ttyUSB0")'
+SERIAL_FOR_URL_INIT = 'serial_for_url("rfc2217://127.0.0.1:8080")'
+
+
+def make_test_file(serial_init: str) -> str:
+    return f"""
             import serial
+            from serial import serial_for_url
             def test_reserial(reserial):
-                s = serial.Serial(port="/dev/ttyUSB0")
+                s = {serial_init}
                 s.write({TEST_TX!r})
                 assert s.in_waiting == {len(TEST_RX)}
                 assert s.read() == {TEST_RX!r}
             def test_reserial2(reserial):
-                s = serial.Serial(port="/dev/ttyUSB0")
+                s = {serial_init}
                 s.write({TEST_TX!r})
                 assert s.read() == {TEST_RX!r}
             """
-TEST_FILE_REPLAY = f"""
+
+
+def make_file_replay(serial_init: str) -> str:
+    return f"""
             import pytest
             import serial
+            from serial import serial_for_url
             def test_reserial(reserial):
-                s = serial.Serial(port="/dev/ttyUSB0")
+                s = {serial_init}
                 s.write({TEST_TX!r})
                 assert s.in_waiting == {len(TEST_RX)}
                 assert s.read() == {TEST_RX!r}
@@ -31,54 +43,96 @@ TEST_FILE_REPLAY = f"""
                 with pytest.raises(serial.PortNotOpenError):
                     s.read()
             def test_reserial2(reserial):
-                s = serial.Serial(port="/dev/ttyUSB0")
+                s = {serial_init}
                 s.write({TEST_TX!r})
                 assert s.read() == {TEST_RX!r}
                 s.close()
                 with pytest.raises(serial.PortNotOpenError):
                     s.write({TEST_TX!r})
             """
-TEST_FILE_BAD_TX = f"""
-                    import serial
-                    def test_reserial(reserial):
-                        s = serial.Serial(port="/dev/ttyUSB0")
-                        s.write({TEST_RX!r})
-                        assert s.read() == {TEST_RX!r}
-                    """
+
+
+def make_file_bad(serial_init: str) -> str:
+    return f"""
+            import serial
+            from serial import serial_for_url
+            def test_reserial(reserial):
+                s = {serial_init}
+                s.write({TEST_RX!r})
+                assert s.read() == {TEST_RX!r}
+            """
+
+
+def make_file_update_existing(serial_init: str) -> str:
+    return f"""
+            import serial
+            from serial import serial_for_url
+            def test_reserial(reserial):
+                s = {serial_init}
+                s.write({2 * TEST_TX})
+                assert s.read() == {2 * TEST_RX}
+                """
+
+
+def make_change_settings_file(serial_init: str) -> str:
+    return f"""
+            import serial
+            from serial import serial_for_url
+            def test_reserial(reserial):
+                s = {serial_init}
+                s.timeout = 1
+        """
+
+
 TEST_JSONL = (
     f'{{"test_reserial": {{"rx": "{TEST_RX_ENC}", "tx": "{TEST_TX_ENC}"}}}}\n'
     f'{{"test_reserial2": {{"rx": "{TEST_RX_ENC}", "tx": "{TEST_TX_ENC}"}}}}\n'
 )
 
 
-def test_record(monkeypatch, pytester):
-    pytester.makepyfile(TEST_FILE)
+@pytest.mark.parametrize(
+    ("serial_init", "SerialClass"),
+    [
+        pytest.param(
+            STANDARD_SERIAL_CONNECTION_INIT,
+            Serial,
+            id="standard serial connection",
+        ),
+        pytest.param(
+            SERIAL_FOR_URL_INIT,
+            RFC2217Serial,
+            id="serial_for_url connection to RFC2217 server",
+        ),
+    ],
+)
+def test_record(serial_init: str, SerialClass, monkeypatch, pytester):
+    pytester.makepyfile(make_test_file(serial_init))
 
-    def patch_write(self: Serial, data: bytes) -> int:
+    def patch_write(self: SerialClass, data: bytes) -> int:
         return len(data)
 
-    def patch_read(self: Serial, size: int = 1) -> bytes:
+    def patch_read(self: SerialClass, size: int = 1) -> bytes:
         return TEST_RX
 
     @property
-    def patch_in_waiting(self: Serial) -> int:
+    def patch_in_waiting(self: SerialClass) -> int:
         return len(TEST_RX)
 
-    def patch_open(self: Serial) -> None:
+    def patch_open(self: SerialClass) -> None:
         self.is_open = True
 
-    def patch_close(self: Serial) -> None:
+    def patch_close(self: SerialClass) -> None:
         self.is_open = False
 
-    monkeypatch.setattr(Serial, "write", patch_write)
-    monkeypatch.setattr(Serial, "read", patch_read)
-    monkeypatch.setattr(Serial, "open", patch_open)
-    monkeypatch.setattr(Serial, "close", patch_close)
-    monkeypatch.setattr(Serial, "in_waiting", patch_in_waiting)
+    monkeypatch.setattr(SerialClass, "write", patch_write)
+    monkeypatch.setattr(SerialClass, "read", patch_read)
+    monkeypatch.setattr(SerialClass, "open", patch_open)
+    monkeypatch.setattr(SerialClass, "close", patch_close)
+    monkeypatch.setattr(SerialClass, "in_waiting", patch_in_waiting)
     result = pytester.runpytest("--record")
 
-    with open("test_record.jsonl", "r") as f:
-        recording = [json.loads(line) for line in f.readlines()]
+    with open("test_record.jsonl") as f:
+        recording = [json.loads(line) for line in f]
 
     expected = [json.loads(line) for line in TEST_JSONL.splitlines()]
 
@@ -86,38 +140,45 @@ def test_record(monkeypatch, pytester):
     assert result.ret == 0
 
 
-def test_update_existing(monkeypatch, pytester):
+@pytest.mark.parametrize(
+    ("serial_init", "SerialClass"),
+    [
+        pytest.param(
+            STANDARD_SERIAL_CONNECTION_INIT,
+            Serial,
+            id="standard serial connection",
+        ),
+        pytest.param(
+            SERIAL_FOR_URL_INIT,
+            RFC2217Serial,
+            id="standard serial connection",
+        ),
+    ],
+)
+def test_update_existing(serial_init: str, SerialClass, monkeypatch, pytester):
     pytester.makefile(".jsonl", test_update_existing=TEST_JSONL)
-    pytester.makepyfile(
-        f"""
-        import serial
-        def test_reserial(reserial):
-            s = serial.Serial(port="/dev/ttyUSB0")
-            s.write({2 * TEST_TX})
-            assert s.read() == {2 * TEST_RX}
-        """
-    )
+    pytester.makepyfile(make_file_update_existing(serial_init))
 
-    def patch_write(self: Serial, data: bytes) -> int:
+    def patch_write(self: SerialClass, data: bytes) -> int:
         return len(data)
 
-    def patch_read(self: Serial, size: int = 1) -> bytes:
+    def patch_read(self: SerialClass, size: int = 1) -> bytes:
         return 2 * TEST_RX
 
-    def patch_open(self: Serial) -> None:
+    def patch_open(self: SerialClass) -> None:
         self.is_open = True
 
-    def patch_close(self: Serial) -> None:
+    def patch_close(self: SerialClass) -> None:
         self.is_open = False
 
-    monkeypatch.setattr(Serial, "write", patch_write)
-    monkeypatch.setattr(Serial, "read", patch_read)
-    monkeypatch.setattr(Serial, "open", patch_open)
-    monkeypatch.setattr(Serial, "close", patch_close)
+    monkeypatch.setattr(SerialClass, "write", patch_write)
+    monkeypatch.setattr(SerialClass, "read", patch_read)
+    monkeypatch.setattr(SerialClass, "open", patch_open)
+    monkeypatch.setattr(SerialClass, "close", patch_close)
     result = pytester.runpytest("--record")
 
-    with open("test_update_existing.jsonl", "r") as f:
-        recording = [json.loads(line) for line in f.readlines()]
+    with open("test_update_existing.jsonl") as f:
+        recording = [json.loads(line) for line in f]
 
     expected_rx_enc = "AQE="
     expected_tx_enc = "AgI="
@@ -132,17 +193,56 @@ def test_update_existing(monkeypatch, pytester):
     assert result.ret == 0
 
 
-def test_replay(pytester):
-    pytester.makefile(".jsonl", test_replay=TEST_JSONL)
-    pytester.makepyfile(TEST_FILE_REPLAY)
-    result = pytester.runpytest()
-    assert result.ret == 0
+@pytest.mark.parametrize(
+    "serial_init",
+    [
+        pytest.param(STANDARD_SERIAL_CONNECTION_INIT, id="standard serial connection"),
+        pytest.param(
+            SERIAL_FOR_URL_INIT, id="serial_for_url connection to RFC2217 server"
+        ),
+    ],
+)
+class TestSerialInit:
+    """Tests parametrized by serial_init only."""
+
+    def test_replay(self, serial_init: str, pytester):
+        pytester.makefile(".jsonl", test_replay=TEST_JSONL)
+        pytester.makepyfile(make_file_replay(serial_init))
+        result = pytester.runpytest()
+        assert result.ret == 0
+
+    def test_invalid_option(self, serial_init: str, pytester):
+        pytester.makepyfile(make_test_file(serial_init))
+        result = pytester.runpytest("--disable-reserial", "--record")
+        result.assert_outcomes(errors=2)
+
+    def test_bad_tx(self, serial_init: str, pytester):
+        pytester.makefile(".jsonl", test_bad_tx=TEST_JSONL)
+        pytester.makepyfile(make_file_bad(serial_init))
+        result = pytester.runpytest()
+        result.assert_outcomes(errors=1, failed=1)
+
+    def test_change_settings(self, serial_init: str, pytester):
+        pytester.makefile(
+            ".jsonl",
+            test_change_settings='{"test_reserial": {"tx": "", "rx": ""}}',
+        )
+        pytester.makepyfile(make_change_settings_file(serial_init))
+        result = pytester.runpytest()
+        assert result.ret == 0
 
 
-def test_dont_patch(pytester):
+@pytest.mark.parametrize(
+    "serial_module",
+    [
+        pytest.param("serial", id="main library"),
+        pytest.param("serial.rfc2217", id="RFC2217 connection"),
+    ],
+)
+def test_dont_patch(serial_module: str, pytester):
     pytester.makepyfile(
-        """
-        from serial import Serial
+        f"""
+        from {serial_module} import Serial
         real_read = Serial.read
         def test_something(reserial):
             assert Serial.read == real_read
@@ -150,19 +250,6 @@ def test_dont_patch(pytester):
     )
     result = pytester.runpytest("--disable-reserial")
     assert result.ret == 0
-
-
-def test_invalid_option(pytester):
-    pytester.makepyfile(TEST_FILE)
-    result = pytester.runpytest("--disable-reserial", "--record")
-    result.assert_outcomes(errors=2)
-
-
-def test_bad_tx(pytester):
-    pytester.makefile(".jsonl", test_bad_tx=TEST_JSONL)
-    pytester.makepyfile(TEST_FILE_BAD_TX)
-    result = pytester.runpytest()
-    result.assert_outcomes(errors=1, failed=1)
 
 
 def test_help_message(pytester):
@@ -175,23 +262,6 @@ def test_help_message(pytester):
             "*--disable-reserial * Disable reserial to allow standard*",
         ]
     )
-    assert result.ret == 0
-
-
-def test_change_settings(pytester):
-    pytester.makefile(
-        ".jsonl",
-        test_change_settings='{"test_reserial": {"tx": "", "rx": ""}}',
-    )
-    pytester.makepyfile(
-        """
-            import serial
-            def test_reserial(reserial):
-                s = serial.Serial(port="/dev/ttyUSB0")
-                s.timeout = 1
-        """
-    )
-    result = pytester.runpytest()
     assert result.ret == 0
 
 
